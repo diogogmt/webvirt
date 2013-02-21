77438882399e2d3f2a7a8eb4bf65072c793f0298
Diogo Monteiro <diogo.gmt@gmail.com>
2013-02-21 13:32:51 -0600

    Added change password functionality to user management. Refactored the user management code to live in its own module.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var express = require('express')
   , di = {}
   , config
   , logger
-  , routes;
+  , routes = {};
 
 
 
@@ -21,7 +21,9 @@ var express = require('express')
 di.config = config = require('./config.js');
 di.logger = logger = require('./logger.js').inject(di);
 di.config.type = "server";
-routes = require('./routes').inject(di);
+
+routes.virt = require('./routes').inject(di);
+routes.userManagement = require('./routes/user-management.js').inject(di);
 
 logger.info("config: ", config);  
 
@@ -53,6 +55,8 @@ app.configure(function () {
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.locals.pretty = true;
+
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
@@ -70,21 +74,20 @@ app.get('/test', function (req, res) {
 
 
 // Virt API
-app.get('/list/vms', routes.listGroup);
-app.get('/list/vms/:ip', routes.listSingle);
-app.get('/list/daemons', routes.listDaemons);
+app.get('/list/vms', routes.virt.listGroup);
+app.get('/list/vms/:ip', routes.virt.listSingle);
+app.get('/list/daemons', routes.virt.listDaemons);
 
-app.get('/stats/version/:ip', routes.version);
-app.get('/stats/cpu/:ip', routes.cpuStats);
-app.get('/stats/mem/:ip', routes.memStats);
+app.get('/stats/version/:ip', routes.virt.version);
+app.get('/stats/cpu/:ip', routes.virt.cpuStats);
+app.get('/stats/mem/:ip', routes.virt.memStats);
 
-app.get('/status/:ip/:name', routes.actions);
-app.get('/start/:ip/:name', routes.actions);
-app.get('/resume/:ip/:name', routes.actions);
-app.get('/suspend/:ip/:name', routes.actions);
-app.get('/shutdown/:ip/:name', routes.actions);
-app.get('/destroy/:ip/:name', routes.actions);
-// app.get('/save/:name', routes.save);
+app.get('/status/:ip/:name', routes.virt.actions);
+app.get('/start/:ip/:name', routes.virt.actions);
+app.get('/resume/:ip/:name', routes.virt.actions);
+app.get('/suspend/:ip/:name', routes.virt.actions);
+app.get('/shutdown/:ip/:name', routes.virt.actions);
+app.get('/destroy/:ip/:name', routes.virt.actions);
 
 
 // Network Scanner API
@@ -92,209 +95,15 @@ app.get("/scan/network", crawler.networkScan);
 app.get("/scan/daemons", crawler.daemonScan);
 
 
-
-var client = require("./db-conn").client;
-
-var Step = require('step');
-
-
-var bcrypt = require('bcrypt-nodejs');
-var crypto = require('crypto');
+// User Management
+app.get('/user/login', routes.userManagement.loginGet);
+app.get('/user/create', routes.userManagement.createGet);
+app.get('/user/changePassword', routes.userManagement.changePasswordGet);
 
 
-app.get('/user/login', function (req, res) {
-  console.log("login route");
-
-  res.render('login', {});
-});
-
-app.get('/user/create', function (req, res) {
-  console.log("login route");
-
-  res.render('create-user', {});
-});
-
-
-
-
-app.post('/user/create', function (req, res) {
-  console.log("route /createUser");
-  var username = req.body['username'];
-  var password = req.body['password'];
-  console.log("username: ", username);
-  console.log("password: ", password);
-  
-  var hashKey = "users:" + username;
-
-  Step([
-    function checkUsername () {
-      console.log("Step checkUsername");
-      client.hlen(hashKey, this);
-    },
-
-    function createUser (err, len) {
-      console.log("Step createUser");
-      console.log("err: ", err);
-      console.log("len: ", len);
-
-      var step = this;
-
-      if (len) {
-        this.exitChain();
-        return;
-      }
-
-      var salt = bcrypt.genSalt(100, function (err, salt) {
-        console.log("salt: ", salt);
-        // var shasum = crypto.createHash('sha512');
-        // var newHash = shasum.update(hash).digest("hex");
-        var scrypt = require("scrypt");
-        var maxtime = 0.1;
-        var maxmem = 0, maxmemfrac = 0.5;
-
-        var saltLength = salt.length;
-        var saltedPass = salt.slice(0,saltLength / 2) + password + salt.slice(saltLength / 2, saltLength);
-        console.log("saltLength: ", saltLength); 
-        console.log("saltedPass: ", saltedPass); 
-
-        scrypt.passwordHash(saltedPass, maxtime, maxmem, maxmemfrac, function(err, scryptHash) {
-          console.log("err: ", err);
-          console.log("scryptHash: ", scryptHash);
-          client.multi()
-            .hset(hashKey, "password", scryptHash)
-            .hset(hashKey, "salt", salt)
-            .exec(step);
-
-    });
-      });
-
-    },
-
-    function confirmCreation (err, status) {
-      console.log("confirmCreation");
-      console.log("args: ", arguments);
-      console.log("err: ", err);
-      console.log("status: ", status);
-
-      res.json({
-        err: err,
-        status: status
-      });
-    }
-
-  ]);
-
-  
-});
-
-app.post('/user/auth', function (req, res) {
-  console.log("route /auth");
-  var username = req.body.username;
-  var password = req.body.password;
-  console.log("req.body: ", req.body);
-  console.log("username: ", username);
-  console.log("password: ", password);
-
-  var hashKey = "users:" + username;
-
-  Step([
-    function getUserInfo () {
-      console.log("Step checkUsername");
-      var step = this;
-      // client.multi()
-      //   .hmget(hashKey, "salt")
-      //   .hmget(hashKey, "password")
-      //   .exec(step);
-      client.hgetall(hashKey, this);
-    },
-
-    function authsUser (err, data) {
-      console.log("Step createUser");
-      console.log("args: ", arguments);
-      var salt = data.salt;
-      var savedPassword = data.password;
-      console.log("err: ", err);
-      console.log("salt: ", salt);
-      console.log("savedPassword: ", savedPassword);
-      console.log("password: ", password);
-
-      var step = this;
-
-      if (err || !salt || ! password) {
-        this.exitChain();
-        return;
-      }
-
-      var scrypt = require("scrypt");
-      var maxtime = 0.1;
-      var maxmem = 0, maxmemfrac = 0.5;
-
-      var saltLength = salt.length;
-      var saltedPass = salt.slice(0,saltLength / 2) + password + salt.slice(saltLength / 2, saltLength);
-      console.log("saltLength: ", saltLength); 
-      console.log("saltedPass: ", saltedPass); 
-
-      scrypt.verifyHash(savedPassword, saltedPass, this); 
-
-    },
-
-    function checkResults (err, result) {
-      console.log("err: ", err);
-      console.log("result: ", result);
-      res.json({
-        err: err,
-        result: result
-      });
-    }
-
-  ]);
-  
-
-  // var scrypt = require("scrypt");
-  // var maxtime = 0.1;
-  // var maxmem = 0, maxmemfrac = 0.5;
-
-
-
-  // var pass = "abc";
-  // scrypt.passwordHash(pass, maxtime, maxmem, maxmemfrac, function(err, scryptHash) {
-  //   console.log("err: ", err);
-  //   console.log("scryptHash: ", scryptHash);
-
-  //   scrypt.verifyHash(scryptHash, pass, function(err, result) {
-  //     console.log("err: ", err);
-  //     console.log("result: ", result);
-  //   });
-  // });
-
-  // scrypt.passwordHash(pass, maxtime, maxmem, maxmemfrac, function(err, scryptHash) {
-  //   console.log("err: ", err);
-  //   console.log("scryptHash: ", scryptHash);
-  
-  //   scrypt.verifyHash(scryptHash, pass, function(err, result) {
-  //     console.log("err: ", err);
-  //     console.log("result: ", result);
-  //   });
-  // });
-
-  // scrypt.passwordHash(pass, maxtime, maxmem, maxmemfrac, function(err, scryptHash) {
-  //   console.log("err: ", err);
-  //   console.log("scryptHash: ", scryptHash);
-  //   scrypt.passwordHash(pass, maxtime, maxmem, maxmemfrac, function(err, newHash) {
-  //     console.log("err: ", err);
-  //     console.log("newHash: ", newHash);
-  //     if (scryptHash === newHash) {
-  //         console.log("PASSWORDS MATCH!");
-  //       } else {
-  //         console.log("PASSWORDS DO NOT MATCH!");
-  //       }
-  //   });
-  // });
-
-
-  // res.json(null);
-
-});
+app.post('/user/changePassword', routes.userManagement.changePassword);
+app.post('/user/create', routes.userManagement.create);
+app.post('/user/auth', routes.userManagement.auth);
 
 
 http.createServer(app).listen(app.get('port'), function(){
