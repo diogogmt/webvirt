4edb916cce5899eb7999c9f26da4407574e9bb04
Diogo Monteiro <diogo.gmt@gmail.com>
2013-02-28 17:31:56 -0600

    Testing new express configuration.

---
+++ server-test.js
@@ -0,0 +1,50 @@
+
+
+/**
+ * Module dependencies.
+ */
+
+
+var express = require('express')
+  , crawler = require('./routes/crawler-routes.js')
+  , http = require('http')
+  , path = require('path')
+  , connect = require('connect')
+
+
+var app = express();
+
+console.log("process.env[NODE_SERVER_TYPE]: ", process.env["NODE_SERVER_TYPE"]);
+
+
+app.configure(function () {
+  app.set('port', 8080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(connect.bodyParser({uploadDir:'./uploads'}));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('test'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.locals.pretty = true;
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.use(require("./lib"));
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+
