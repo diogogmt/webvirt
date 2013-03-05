070efd7b0cc699f2617910007b1a7bdb2cbe0df2
Diogo Monteiro <diogo.gmt@gmail.com>
2013-03-04 20:34:36 -0600

    Updated Daemon Management screen. Switched from jquery to underscore templates for the view rendering.

--- public/js/app/daemon-management/models/daemon-model.js
+++ public/js/app/daemon-management/models/daemon-model.js
@@ -14,39 +14,16 @@ app.Daemon = Backbone.Model.extend({
 
   idAttribute: "customId",
 
-  urlRoot: "/daemons",
-
   initialize: function () {
     console.log("Daemon - initialize");
   },
 
-  save: function () {
-    console.log("Daemon save");
-    this.urlRoot = "/daemons" + (this.isNew() ? "/add/" : "/update");
-    console.log("this.urlRoot: ", this.urlRoot);
-
-    Backbone.Model.prototype.save.apply(this, this);
-  },
-
-  destroy: function () {
-    console.log("Daemon destroy");
-    this.urlRoot = "/daemons/delete";
-    console.log("this.urlRoot: ", this.urlRoot);
-
-    Backbone.Model.prototype.destroy.apply(this, this);
-  },
-
   validate: function (attrs, options) {
-    console.log("Daemon - validate");
-    console.log("args: ", arguments);
-    var ip = attrs["ip"] || null;
-    var match = ip && ip.match(/^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/)
-    var len = match && match.length || 0;
-    console.log("match: ", match);
-    console.log("match.length: ", len);
+    var ip = attrs["ip"] || null
+      , match = ip && ip.match(/^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/)
+      , len = match && match.length || 0;
     
     if (len != 1) {
-      console.log("IP is not valid");
       return "Invalid IP";
     }
   }
