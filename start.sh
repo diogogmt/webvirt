c7d08bee4f652a38983efe40f358541a345a69d4
Diogo Monteiro <diogo.gmt@gmail.com>
2013-03-13 16:59:57 -0700

    Modified start script to fire a redis-server instance.

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 if [ -z "$MY_PATH" ] ; then
@@ -5,4 +7,28 @@ if [ -z "$MY_PATH" ] ; then
 fi
 echo "$MY_PATH"
 export NODE_PATH=$MY_PATH
+
+REDIS_IP="127.0.0.1"
+REDIS_PORT=6379
+REDIS_DIR="../redis/redis-2.6.11/src"
+
+NODE="../nodejs/node-v0.10.0-linux-x64/bin/node"
+
+export NODE="../nodejs/node-v0.10.0-linux-x64/bin"
+export NODE_PATH="../nodejs/node-v0.10.0-linux-x64/lib"
+
+echo "PATH: $PATH"
+
+export PATH=$PATH:$NODE_PATH:$NODE
+
+echo "Starting redis-server"
+echo "REDIS_DIR: $REDIS_DIR"
+$REDIS_DIR/redis-server --port $REDIS_PORT
+
+echo -e "\n---------------\n"
+
+echo $PWD
+cd $MY_PATH
+pwd
+
 nodemon server.js
