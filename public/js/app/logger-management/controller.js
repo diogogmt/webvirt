5df2868e190d263dbd6c17a56b7a6c368dd2b0b5
Diogo Monteiro <diogo.gmt@gmail.com>
2013-03-14 11:49:29 -0500

    Fixed issue with socket host connection.

--- public/js/app/logger-management/controller.js
+++ public/js/app/logger-management/controller.js
@@ -37,7 +37,7 @@ app.Controller = Backbone.View.extend({
     self.collections['info'].fetch();
 
     // Create socketIO connection
-    this.logger = io.connect('http://127.0.0.1/logger');
+    this.logger = io.connect('/logger');
     // Incoming log via socket connection
     this.logger.on('newLog', function (data) {
       var log = new app.Log({
