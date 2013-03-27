0296841e81085c0d58c396a51ae6a09f3e110b08
Diogo Monteiro <diogo.gmt@gmail.com>
2013-03-27 11:21:18 -0500

    Removed redis from webvirt-node start script. Started to use forever instead of nodemon to start the webvirt services.

--- manager_start.sh
+++ manager_start.sh
@@ -1,4 +1,14 @@
 export NODE_TYPE="server"
 export NODE_PORT=3000
 
+REDIS_DIR="redis/redis-2.6.11/src"
+REDIS_CONF="config/redis.conf"
+
+# Check if redis-server is already running
+netstat -atnp 2>/dev/null | grep -qE ".*${REDIS_PORT}.*"redis-server
+if [ $? -ne 0 ]; then
+  echo "Starting redis server"
+  $REDIS_DIR/redis-server $REDIS_CONF
+fi
+
 ./start.sh
--- start.sh
+++ start.sh
@@ -7,19 +7,10 @@ if [ -z "$MY_PATH" ] ; then
   exit 1 
 fi
 
-REDIS_DIR="redis/redis-2.6.11/src"
-REDIS_CONF="config/redis.conf"
-
 export NODE="nodejs/node-v0.10.0-linux-x64/bin"
 export NODE_PATH="nodejs/node-v0.10.0-linux-x64/lib"
 export PATH=$PATH:$NODE_PATH:$NODE
 export NODE_PATH=$MY_PATH
 
-# Check if redis-server is already running
-netstat -atnp 2>/dev/null | grep -qE ".*${REDIS_PORT}.*"redis-server
-if [ $? -ne 0 ]; then
-  echo "Starting redis server"
-  $REDIS_DIR/redis-server $REDIS_CONF
-fi
 
-node_modules/nodemon/nodemon.js server.js
+node_modules/forever/bin/forever start server.js
