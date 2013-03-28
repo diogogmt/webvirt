c0196c7fea5b20edd978000630328089ecdb22b1
ksedge <kieran.sedgwick@gmail.com>
2013-03-28 13:17:35 -0400

    added stop script

---
+++ stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Save the absolute path of the project's root
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  exit 1 
+fi
+
+export NODE="nodejs/node-v0.8.10-linux-x64/bin"
+export NODE_PATH="nodejs/node-v0.08.10-linux-x64/lib"
+export PATH=$NODE_PATH:$NODE:$PATH
+export NODE_PATH=$MY_PATH
+
+
+node_modules/forever/bin/forever stop server.js
